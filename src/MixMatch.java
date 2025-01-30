0429f229845a6b4ad2a5ce9f6b526b93011c7f1d
A. Mal <jmaldonado250@icloud.com>
2025-01-30 21:05:18 +0000

    First commit. Single print method and Multiple method print used.

---
+++ src/MixMatch.java
@@ -0,0 +1,21 @@
+public class MixMatch
+{
+    public static void main(String[] args)
+    {
+        String name = "Angel";
+        char letter = 'A';
+        short numShort = 1;
+        int numInt = 3;
+        long numLong = 7;
+        double numDouble = 9.53;
+        boolean bool = true;
+
+        System.out.println("Single print method: ");
+        System.out.println("Name: " + name);
+        System.out.println("Char: " + letter);
+        System.out.println("Short: " + numShort);
+
+        System.out.println("Multiple variables method:");
+        System.out.println("Int: " + numInt + " Long: " + numLong + " Float: " + numLong + " Double: " + numDouble + " Boolean: " + bool);
+    }
+}
